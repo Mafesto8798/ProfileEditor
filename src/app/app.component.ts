38c85fe4fbfd643db445c247c2c08fd37d4c68fe
Kenny <75293069+Mafesto8798@users.noreply.github.com>
2022-09-29 20:51:04 -0400

    First push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'profileEditor';
+  title = 'Profile Editor';
+  displayItem: User = {
+    name: 'Joe',
+    age: 24,
+    favoriteColor: 'Orange',
+    favoriteAnimal: 'Dog',
+  };
+
+  displayEdit: boolean = false;
+  toggleEdit(): void {
+    this.displayEdit = !this.displayEdit;
+  }
 }
